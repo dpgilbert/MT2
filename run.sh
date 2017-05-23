c5bc5eb68869c2ba4d34e00e4aaeb201bfd5cae0
dpg <dgilbert@physics.ucsd.edu>
2017-05-23 14:42:52 -0700

    run script modifications to work with new exes

--- run.sh
+++ run.sh
@@ -9,18 +9,18 @@ declare -a T2Samples=(T2bW T2bb T2bt T2qq)
 declare -a BackgroundSamples=(dyjets qcd singletop ttsl ttdl ttg tth ttw ttz tttt twz wjets ww zinv)
 
 #for SAMPLE in ${BackgroundSamples[@]}; do
-#    eval 'nohup nice -n 10 ./MyLooper < UnskimmedRunLists/${SAMPLE}.txt >& logs/${SAMPLE}_log.txt &'
+#    eval 'nohup nice -n 10 ./MyLooper.exe < UnskimmedRunLists/${SAMPLE}.txt >& logs/${SAMPLE}_log.txt &'
 #done
 
 for SAMPLE in ${T1Samples[@]}; do
-    eval 'nohup nice -n 10 ./MyLooper < SignalRunLists/${SAMPLE}_mg2200_mx200.txt >& logs/${SAMPLE}_mg2200_mx200_log.txt &'
-    eval 'nohup nice -n 10 ./MyLooper < SignalRunLists/${SAMPLE}_mg1700_mx1400.txt >& logs/${SAMPLE}_mg1700_mx1400_log.txt &'
+    eval 'nohup nice -n 10 ./MyLooper.exe < SignalRunLists/${SAMPLE}_mg2200_mx200.txt >& logs/${SAMPLE}_mg2200_mx200_log.txt &'
+    eval 'nohup nice -n 10 ./MyLooper.exe < SignalRunLists/${SAMPLE}_mg1700_mx1400.txt >& logs/${SAMPLE}_mg1700_mx1400_log.txt &'
 done
 
 for SAMPLE in ${T2Samples[@]}; do
-    eval 'nohup nice -n 10 ./MyLooper < SignalRunLists/${SAMPLE}_mst800_mx600.txt >& logs/${SAMPLE}_mst800_mx600_log.txt &'
-    eval 'nohup nice -n 10 ./MyLooper < SignalRunLists/${SAMPLE}_mst1200_mx200.txt >& logs/${SAMPLE}_mst1200_mx200_log.txt &'
+    eval 'nohup nice -n 10 ./MyLooper.exe < SignalRunLists/${SAMPLE}_mst800_mx600.txt >& logs/${SAMPLE}_mst800_mx600_log.txt &'
+    eval 'nohup nice -n 10 ./MyLooper.exe < SignalRunLists/${SAMPLE}_mst1200_mx200.txt >& logs/${SAMPLE}_mst1200_mx200_log.txt &'
 done
 
-eval 'nohup nice -n 10 ./MyLooper < SignalRunLists/T2cc_mst600_mx550.txt >& logs/T2cc_mst600_mx550_log.txt &'
+eval 'nohup nice -n 10 ./MyLooper.exe < SignalRunLists/T2cc_mst600_mx550.txt >& logs/T2cc_mst600_mx550_log.txt &'
 
